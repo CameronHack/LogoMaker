fb528ac79f4e0c35cfba19645ec621e2253af319
Cam <camhack67@gmail.com>
2023-08-20 12:34:17 -0400

    inquirer

--- index.js
+++ index.js
@@ -1,10 +1,49 @@
 const fs = require('fs')
+// const inquirer = require('inquirer');
 let SVG = require('./lib/svg')
+
+// inquirer
+//     .prompt([
+//         {
+//             type: 'list',
+//             name: 'logoShape',
+//             message: "what shape do you want",
+//             choices: [
+//                 'circle',
+//                 'square',
+//                 'triangle'],
+//         },
+//         {
+//             type: 'input',
+//             name: 'title',
+//             message: "Enter logo text",
+//         },
+//         {
+//             type: 'input',
+//             name: 'shapeColor',
+//             message: "What color do you want your shape background",
+//         },
+//         {
+//             type: 'input',
+//             name: 'textColor',
+//             message: "What color do you want your text",
+//         },
+//     ])
+//     .then((answers) => {
+//         const { logoShape, title, shapeColor, textColor } = answers;
+//     })
+//     .catch((err) => {
+//         if (err) {
+//             console.log(err)
+//         }
+//     });
+
+
 
 let newShape = new SVG()
 
 fs.writeFile(`./output/Logo.svg`, newShape.render(), (err) => {
-    if(err) [
+    if(err) {
         console.log(err)
-    ]
+    }
 })
